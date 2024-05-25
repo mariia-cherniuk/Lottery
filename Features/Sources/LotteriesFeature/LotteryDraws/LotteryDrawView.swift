6032c6a6f977e36130c184489ef667c82d48efcd
mariia-cherniuk <marydort1@gmail.com>
2024-05-25 16:10:04 +0100

    Improve UI

--- Features/Sources/LotteriesFeature/LotteryDraws/LotteryDrawView.swift
+++ Features/Sources/LotteriesFeature/LotteryDraws/LotteryDrawView.swift
@@ -9,6 +9,20 @@ struct LotteryDrawView: View {
     }
     
     var body: some View {
-        Text(viewModel.date)
+        HStack(spacing: .spacing.large) {
+            Text(viewModel.date)
+            Spacer()
+        }
+        .contentShape(Rectangle())
+        .padding(.large)
+        .overlay(overlay)
+    }
+}
+
+private extension LotteryDrawView {
+    
+    var overlay: some View {
+        RoundedRectangle(cornerRadius: 8)
+            .stroke(Color.purple, lineWidth: 1)
     }
 }
