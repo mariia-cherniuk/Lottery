1760a6ca0635e9878b6565d61cb4ea8aebedde36
mariia-cherniuk <marydort1@gmail.com>
2024-05-25 22:59:27 +0100

    Fetch lotteries from local store when notConnectedToInternet

--- Domain/Sources/LotteriesDomain/UseCases/LotteryDrawsUseCase.swift
+++ Domain/Sources/LotteriesDomain/UseCases/LotteryDrawsUseCase.swift
@@ -10,40 +10,22 @@ final class LotteryDrawsUseCase: LotteryDrawsUseCaseProtocol {
     private let path: String = "/mariia-cherniuk/Lottery/master/Resources/lotteries.json"
     private let dataLoader: DataLoading
     private let lotteriesStorage: LotteriesStorable
-    private let transformer: LotteryDrawsTransformer
-    
+
     init(dataLoader: DataLoading,
-         lotteriesStorage: LotteriesStorable,
-         transformer: LotteryDrawsTransformer = LotteryDrawsTransformer()) {
+         lotteriesStorage: LotteriesStorable) {
         self.dataLoader = dataLoader
         self.lotteriesStorage = lotteriesStorage
-        self.transformer = transformer
     }
     
     func fetch() async throws -> LotteriesResponse {
-        let data = try await dataLoader.fetch(resource: Resource(path: path, method: .GET))
-        return try transformer.transform(data: data)
-    }
-}
-
-final class LotteryDrawsTransformer {
-    
-    func transform(data: Data) throws -> LotteriesResponse {
         do {
-            let decoded = try JSONDecoder().decode(LotteriesResponse.self, from: data)
-            return decoded
+            let data = try await dataLoader.fetch(resource: Resource(path: path, method: .GET))
+            return try JSONDecoder().decode(LotteriesResponse.self, from: data)
+        } catch NetworkError.notConnectedToInternet {
+            let lotteries = try lotteriesStorage.fetchAll()
+            return LotteriesResponse(draws: lotteries)
         } catch {
-            throw map(error: error)
-        }
-    }
-    
-    private func map(error: Error) -> DomainError {
-        guard let error = error as? NetworkError else { return .other }
-        switch error {
-        case .notConnectedToInternet:
-            return .notConnectedToInternet
-        default:
-            return .other
+            throw DomainError.other
         }
     }
 }
