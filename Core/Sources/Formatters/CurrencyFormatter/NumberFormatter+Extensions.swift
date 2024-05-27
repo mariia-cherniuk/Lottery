78a97eaebc1a2856f1db0796dd8a6460c052744d
mariia-cherniuk <marydort1@gmail.com>
2024-05-27 13:29:58 +0100

    Fix fraction digits

--- Core/Sources/Formatters/CurrencyFormatter/NumberFormatter+Extensions.swift
+++ Core/Sources/Formatters/CurrencyFormatter/NumberFormatter+Extensions.swift
@@ -5,8 +5,8 @@ extension NumberFormatter {
     public static var currency: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
-        formatter.minimumFractionDigits = 2
-        formatter.maximumFractionDigits = 2
+        formatter.minimumFractionDigits = 0
+        formatter.maximumFractionDigits = 0
         return formatter
     }
 }
