c45a397e1def9fdab6a979d222b37d2d30f39060
mariia-cherniuk <marydort1@gmail.com>
2024-05-27 14:22:37 +0100

    Update test

--- Features/Tests/LotteriesFeatureTests/LotteryDraws/LotteryDrawCellViewModelTests.swift
+++ Features/Tests/LotteriesFeatureTests/LotteryDraws/LotteryDrawCellViewModelTests.swift
@@ -2,11 +2,12 @@ import XCTest
 
 @testable import LotteriesFeature
 
-final class LotteryDrawTests: XCTestCase {
+final class LotteryDrawCellViewModelTests: XCTestCase {
     
     func testLotteryDraw() {
         let lotteryDraw = LotteryDrawCellViewModel(lottery: .fixture(id: "id-1", drawDate: Date.fixture()))
         XCTAssertEqual(lotteryDraw.id, "id-1")
         XCTAssertEqual(lotteryDraw.date, "15 May 2023")
+        XCTAssertEqual(lotteryDraw.topPrize, "£40,000,000")
     }
 }
