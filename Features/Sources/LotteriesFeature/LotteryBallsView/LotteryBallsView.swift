334dc9b8430a090fcfa7b9aff2844447f628d93c
mariia-cherniuk <marydort1@gmail.com>
2024-05-27 13:39:13 +0100

    Rethink for each

--- Features/Sources/LotteriesFeature/LotteryBallsView/LotteryBallsView.swift
+++ Features/Sources/LotteriesFeature/LotteryBallsView/LotteryBallsView.swift
@@ -19,14 +19,8 @@ private extension LotteryBallsView {
     
     var content: some View {
         HStack {
-            
-            ///ForEach requires each element to have a unique identifier to distinguish between different items.
-            ///When you use id: \.self, SwiftUI uses the element itself as the identifier.
-            ///To ensure each item has a unique identifier, I decided to wrap items in a structure that includes a unique identifier.
-            
-            ForEach(Array(viewModels.enumerated()), id: \.offset) { index, viewModel in
+            ForEach(viewModels, id: \.number) { viewModel in
                 BallView(viewModel: viewModel)
-                 //   .frame(width: 50, height: 50)
             }
         }
         .padding(.large)
